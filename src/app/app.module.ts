51640f691b21449cccfe3e2f1037494dc1be8363
Juan Rodriguez <rtjuansebastian@gmail.com>
2019-08-08 17:52:35 -0500

    Se agregaron dependencias para Bootstrap y Font-awesome y se crearon componentes para cabecera y pie de pagina.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
+//Angular imports
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+//App imports
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//Icons imports
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+//Components imports
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+library.add(fas);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
